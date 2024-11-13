bab717f0f77e1877d7adba1b5e403bcaba7d6e61
Cole Vick <cole.d.vick@gmail.com>
2024-11-13 09:33:54 -0600

    call abstraction and run BMC

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use array_axioms::ArrayLanguage;
 use clap::Parser;
-use egg::RecExpr;
-use smt2parser::{concrete::SyntaxBuilder, vmt::VMTModel, CommandStream};
+use smt2parser::{get_commands, vmt::VMTModel};
 use z3::{Config, Context, Solver};
 
 mod array_axioms;
-mod abstract_rewriter;
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -20,39 +17,33 @@ struct Options {
 
 }
 
-
 fn main() {
     let options = Options::parse();
-    let file = std::io::BufReader::new(std::fs::File::open(options.filename.clone()).unwrap());
-    let command_stream = CommandStream::new(file, SyntaxBuilder, Some(options.filename));
-    let mut commands = vec![];
-    for result in command_stream {
-        match result {
-            Ok(command) => commands.push(command),
-            Err(_) => todo!(),
-        }
-    }
-    let vmt_model = VMTModel::checked_from(commands).unwrap();
+    let content = std::io::BufReader::new(std::fs::File::open(options.filename.clone()).unwrap());
+    let commands = get_commands(content, options.filename);
+    let concrete_vmt_model = VMTModel::checked_from(commands).unwrap();
+    let abstract_vmt_model = concrete_vmt_model.abstract_array_theory();
     let config: Config = Config::new();
     let context: Context = Context::new(&config);
-    let solver = Solver::new(&context);
-
     for depth in 0..10 {
-        let smt = vmt_model.unroll(depth);
-        solver.push();
-        solver.from_string(smt.to_smtlib2());
-        let _ = solver.check();
-        let model = solver.get_model().unwrap();
-        let array_language_expr = get_array_language_expression(model);
-        let expr: RecExpr<ArrayLanguage> = array_language_expr.parse().unwrap();
-
+        for d in 0..2 {
+            let smt = abstract_vmt_model.unroll(depth);
+            let solver = Solver::new(&context);
+            solver.from_string(smt.to_smtlib2());
+            match solver.check() {
+                z3::SatResult::Unsat => break, // Ruled out all counterexamples of this depth.
+                z3::SatResult::Unknown => todo!(),
+                z3::SatResult::Sat => {
+                    // find Array theory fact that rules out counterexample
+                    let model = solver.get_model().unwrap();
+                    println!("{}", model);
+                    // Model to Egraph
+                    // Find one or many theory violations
+                    // Add violations as facts
+                },
+            }
+            
+        }
+        
     }
-
-
-}
-
-fn get_array_language_expression(model: z3::Model<'_>) -> String {
-    // Put all equalities in the model and saturate. 
-    // Since f
-    todo!()
 }
