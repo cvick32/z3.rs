520536ac3de6bed905b9540293e7ea08c2b33d35
Cole Vick <cole.d.vick@gmail.com>
2024-11-14 16:11:08 -0600

    add Model->Egraph in main loop

--- src/main.rs
+++ src/main.rs
@@ -27,25 +27,19 @@ fn main() {
     let config: Config = Config::new();
     let context: Context = Context::new(&config);
 
-    let mut egraph: egg::EGraph<ArrayLanguage, ()> = egg::EGraph::new(());
-
-    for depth in 0..10 {
+    for depth in 0..3 {
         println!("STARTING BMC FOR DEPTH {}", depth);
         for _d in 0..1 {
             // Currently run once, this will eventually run until UNSAT
             let smt = abstract_vmt_model.unroll(depth);
             let solver = Solver::new(&context);
             solver.from_string(smt.to_smtlib2());
-            println!("{:#?}", smt.get_assert_terms());
-
+            let mut egraph: egg::EGraph<ArrayLanguage, ()> = egg::EGraph::new(());
             for term in smt.get_assert_terms() {
                 println!("{term}");
                 egraph.add_expr(&term.parse().unwrap());
             }
-
             println!("{:?}", egraph.dump());
-            egraph.dot().to_pdf("babies_first_egraph.pdf").unwrap();
-
             match solver.check() {
                 z3::SatResult::Unsat => {
                     println!("RULED OUT ALL COUNTEREXAMPLES OF DEPTH {}", depth);
@@ -59,10 +53,42 @@ fn main() {
                 z3::SatResult::Sat => {
                     // find Array theory fact that rules out counterexample
                     let model = solver.get_model().unwrap();
-                    todo!("{}", model);
-                    // Model to Egraph
-                    // Find one or many theory violations
-                    // Add violations as facts
+                    println!("{}", model);
+
+                    for func_decl in model.iter() {
+                        if func_decl.arity() == 0 {
+                            // VARIABLE
+                            let func_decl_ast = func_decl.apply(&[]); // Apply no arguments to the constant so we can call get_const_interp.
+                            let var_id = egraph.add_expr(&func_decl.name().parse().unwrap());
+                            let value = model
+                                .get_const_interp(&func_decl_ast)
+                                .expect("Model failure.");
+                            let value_id = egraph.add_expr(&value.to_string().parse().unwrap());
+                            egraph.union(var_id, value_id);
+                        } else {
+                            // FUNCTION DEF
+                            println!("{}", func_decl.name());
+                            let interpretation = model.get_func_interp(&func_decl).unwrap();
+                            for entry in interpretation.get_entries() {
+                                let function_call = format!(
+                                    "({} {})",
+                                    func_decl.name(),
+                                    entry
+                                        .get_args()
+                                        .iter()
+                                        .map(ToString::to_string)
+                                        .collect::<Vec<_>>()
+                                        .join(" ")
+                                );
+                                let function_id = egraph.add_expr(&function_call.parse().unwrap());
+                                let value_id = egraph
+                                    .add_expr(&entry.get_value().to_string().parse().unwrap());
+                                egraph.union(function_id, value_id);
+                            }
+                        }
+                    }
+                    egraph.rebuild();
+                    println!("{:?}", egraph.dump());
                 }
             }
         }
